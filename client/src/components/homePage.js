f593a5638f56ee3e607f03df24d02219bb8bd2be
htunhtunhtet <ryanhhh91@gmail.com>
2018-04-04 13:08:26 +0800

    class to className

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -12,7 +12,7 @@ class HomePage extends Component {
     render(){
         return(
             <MuiThemeProvider>
-                <div class="page-wrapper">
+                <div className="page-wrapper">
                     <AppBar
                         title="Readable"
                         iconClassNameRight="muidocs-icon-navigation-expand-more"
